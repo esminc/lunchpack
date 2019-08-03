e87c0f0e1b9a82ae4b3fd5a6029e294b1e5e4a91
wai-doi <yusuke.doi@esm.co.jp>
2019-08-04 05:57:26 +0900

    トップページを形だけ作った

---
+++ app/controllers/lunches_controller.rb
@@ -0,0 +1,4 @@
+class LunchesController < ApplicationController
+  def new
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,10 @@ module Lunchpack
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
