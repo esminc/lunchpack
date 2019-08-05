6a5f5c429edabe1d4e1d0defb456dc79a27ee848
wai-doi <yusuke.doi@esm.co.jp>
2019-08-05 14:42:25 +0900

    member scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :members
   get 'home/index'
   # devise_for :users
   devise_for :users, controllers: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_213618) do
+ActiveRecord::Schema.define(version: 2019_08_05_053712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.string "hundle_name"
+    t.string "real_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
