325fb39102aba59866e82980a9cf9e9a278503b0
wai-doi <yusuke.doi@esm.co.jp>
2019-08-06 17:07:14 +0900

    Memberのseed作成した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/member.csv', headers: true) do |row|
+  Member.create!(
+    hundle_name: row['GitHub'],
+    real_name: row['氏名 (本名)'].tr(' ', ''),
+  )
+end
