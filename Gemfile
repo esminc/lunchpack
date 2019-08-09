20b4a2da70ee66076551297d3e03d66b2e6c78f6
wai-doi <yusuke.doi@esm.co.jp>
2019-08-09 17:24:48 +0900

    メンバー管理機能に関するテストを書いた

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-rails'
-  gem 'rspec-rails', '~> 3.6'
+  gem 'rspec-rails', '~> 3.8.0'
+  gem 'factory_bot_rails', '~> 4.11'
 end
 
 group :development do
@@ -64,6 +65,14 @@ group :development do
   gem 'spring-commands-rspec'
 end
 
+group :test do
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '>= 2.15'
+  gem 'selenium-webdriver'
+  # Easy installation and use of chromedriver to run system tests with Chrome
+
+  gem "webdrivers", "~> 3.0"
+end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/system/members.spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe 'メンバー管理機能', type: :system do
+  before do
+    FactoryBot.create(:member)
+    sign_in FactoryBot.create(:user)
+    visit root_path
+    click_link 'メンバー'
+  end
+
+  describe '一覧表示機能' do
+    it 'ハンドルネームと氏名が表示されているか' do
+      expect(page).to have_content 'taro'
+      expect(page).to have_content '山田太郎'
+    end
+  end
+
+  describe '新規作成機能' do
+    it '新規に追加できるか' do
+      click_link 'New Member'
+      fill_in 'member[hundle_name]', with: 'hanako'
+      fill_in 'member[real_name]', with: '山田花子'
+      click_button 'Create Member'
+      expect(page).to have_content 'hanako'
+      expect(page).to have_content '山田花子'
+    end
+  end
+
+  describe '編集機能' do
+    it 'ハンドルネームと氏名を編集できるか' do
+      click_link 'Edit'
+      fill_in 'member[hundle_name]', with: 'taro3'
+      fill_in 'member[real_name]', with: '山下太郎'
+      click_button 'Update Member'
+      expect(page).to have_content 'taro3'
+      expect(page).to have_content '山下太郎'
+    end
+  end
+
+  describe '削除機能' do
+    it '削除できるか' do
+      click_link 'Destroy'
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to_not have_content 'taro'
+      expect(page).to_not have_content '山田太郎'
+    end
+  end
+end
