ff1c16151c734b79613976328033ba3ca310050b
wai-doi <yusuke.doi@esm.co.jp>
2019-08-09 18:41:28 +0900

    名前を選択すると同じプロジェクトのメンバーの表示が消えるテストを書いた

--- spec/system/lunch_spec.rb
+++ spec/system/lunch_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 describe '3人組を探す機能' do
   before do
-    FactoryBot.create(:member)
+    project = FactoryBot.create(:project)
+    FactoryBot.create(:member, projects: [project])
+    FactoryBot.create(:member, real_name: '鈴木一郎', projects: [project])
     sign_in FactoryBot.create(:user)
     visit root_path
   end
+
   describe 'メンバー選択' do
     it '名前をクリックすると枠に移動するか' do
-      find('.member-name').click
+      find('.member-name', text: '山田太郎').click
       expect(find('.selected-member-box:first-child')).to have_content('山田太郎')
+      expect(find('#members-list')).to_not have_content('山田太郎')
+    end
+
+    it '名前をクリックすると同じプロジェクトのメンバーの表示が消えるか' do
+      find('.member-name', text: '山田太郎').click
+      expect(find('#members-list')).to_not have_content('鈴木一郎')
     end
   end
 end
