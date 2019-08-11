4fdebbcf7d3a0df1b2cc50386bb5f9e34673d96e
wai-doi <yusuke.doi@esm.co.jp>
2019-08-12 05:28:35 +0900

    追加、編集、削除ボタンを作った

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active(path)
     request.path == path ? 'active': ''
   end
+
+  def project_list(member)
+    if member.projects.present?
+      member.projects.pluck(:name).join(',')
+    else
+      '-'
+    end
+  end
 end
