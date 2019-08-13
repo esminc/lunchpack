e88e81654a7e1e2b474f67060a5b06883787d08f
wai-doi <yusuke.doi@esm.co.jp>
2019-08-13 12:50:36 +0900

    壊れていたテストを修正した

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -16,25 +16,25 @@ describe 'プロジェクト管理機能' do
 
   describe '新規作成機能' do
     it '新規に追加できるか' do
-      click_link 'New Project'
+      find('.new-btn').click
       fill_in 'project[name]', with: 'プロダクト'
-      click_button 'Create Project'
+      find('.submit-btn').click
       expect(page).to have_content 'プロダクト'
     end
   end
 
   describe '編集機能' do
     it '名前が編集できるか' do
-      click_link 'Edit'
+      find('.edit-btn').click
       fill_in 'project[name]', with: 'eiwasan'
-      click_button 'Update Project'
+      find('.submit-btn').click
       expect(page).to have_content 'eiwasan'
     end
   end
 
   describe '削除機能' do
     it '削除できるか' do
-      click_link 'Destroy'
+      find('.delete-btn').click
       page.driver.browser.switch_to.alert.accept
       expect(page).to_not have_content 'eiwakun'
     end
