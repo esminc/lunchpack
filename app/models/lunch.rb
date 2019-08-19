9470f61cccc377ba2f2574023dc11fb990d676fe
wai-doi <yusuke.doi@esm.co.jp>
2019-08-19 18:35:55 +0900

    ランチに行く人が３人でないといけないvaldiationを設定

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -1,3 +1,10 @@
 class Lunch < ApplicationRecord
   has_and_belongs_to_many :members
+  validate :must_have_three_members
+
+  def must_have_three_members
+    unless members.size == 3
+      errors.add(:members, '3人のメンバーを入力してください')
+    end
+  end
 end
