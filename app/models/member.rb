b2fdc4011ddb934985445006c72d09f6e8361aea
wai-doi <yusuke.doi@esm.co.jp>
2019-08-19 16:09:55 +0900

    email属性にunique制約をつけた

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,5 @@ class Member < ApplicationRecord
   has_many :assignments
   has_many :projects, through: :assignments
   has_and_belongs_to_many :lunches
+  validates :email, uniqueness: true, allow_nil: true
 end
