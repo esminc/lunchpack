bff39055bee87cdcb8a41f6ce118dc20dc0d19f1
wai-doi <yusuke.doi@esm.co.jp>
2019-08-20 14:33:21 +0900

    メンバー数を定数にした

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -1,10 +1,12 @@
 class Lunch < ApplicationRecord
   has_and_belongs_to_many :members
-  validate :must_have_three_members
+  validate :must_have_max_members
 
-  def must_have_three_members
-    unless members.size == 3
-      errors.add(:members, '3人のメンバーを入力してください')
+  MEMBERS_NUMBER = 3
+
+  def must_have_max_members
+    unless members.size == MEMBERS_NUMBER
+      errors.add(:members, "#{MEMBERS_NUMBER}人のメンバーを入力してください")
     end
   end
 end
