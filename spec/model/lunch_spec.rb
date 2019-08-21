1df3384b0f4aaf3506e8e1618c0ee290d1d16bdb
wai-doi <yusuke.doi@esm.co.jp>
2019-08-21 18:43:55 +0900

    現在のクウォーターのランチだけをとるscopeのテスト

--- spec/model/lunch_spec.rb
+++ spec/model/lunch_spec.rb
@@ -7,4 +7,16 @@ describe Lunch do
     lunch = build(:lunch, members: [member1, member2])
     expect(lunch).to_not be_valid
   end
+
+  describe 'scope' do
+    it 'in_current_quarter' do
+      member1 = create(:member, real_name: '鈴木一郎')
+      member2 = create(:member, real_name: '鈴木二郎')
+      member3 = create(:member, real_name: '鈴木三郎')
+      create(:lunch, members: [member1, member2, member3])
+      create(:lunch, date: Date.today.next_year, members: [member1, member2, member3])
+      expect(Lunch.count).to eq 2
+      expect(Lunch.in_current_quarter.count).to eq 1
+    end
+  end
 end
