d0e888b55a0f14f23fbd8b3bda263c4e98216e56
wai-doi <yusuke.doi@esm.co.jp>
2019-08-29 10:59:32 +0900

    includeを使ったscopeのテストに修正

--- spec/model/lunch_spec.rb
+++ spec/model/lunch_spec.rb
@@ -9,14 +9,25 @@ describe Lunch do
   end
 
   describe 'scope' do
-    it 'in_current_quarter' do
-      member1 = create(:member, real_name: '鈴木一郎')
-      member2 = create(:member, real_name: '鈴木二郎')
-      member3 = create(:member, real_name: '鈴木三郎')
-      create(:lunch, members: [member1, member2, member3])
-      create(:lunch, date: Date.today.next_year, members: [member1, member2, member3])
-      expect(Lunch.count).to eq 2
-      expect(Lunch.in_current_quarter.count).to eq 1
+    describe 'in_current_quarter' do
+      let(:member1) { create(:member, real_name: '鈴木一郎') }
+      let(:member2) { create(:member, real_name: '鈴木二郎') }
+      let(:member3) { create(:member, real_name: '鈴木三郎') }
+
+      it '今日行ったランチは含まれること' do
+        today_lunch = create(:lunch, members: [member1, member2, member3])
+        expect(Lunch.in_current_quarter).to include today_lunch
+      end
+
+      it '3ヶ月前行ったランチは含まれないこと' do
+        three_month_ago_lunch = create(:lunch, date: Date.today.prev_month(3), members: [member1, member2, member3])
+        expect(Lunch.in_current_quarter).to_not include three_month_ago_lunch
+      end
+
+      it '去年行ったランチは含まれないこと' do
+        last_year_lunch = create(:lunch, date: Date.today.last_year, members: [member1, member2, member3])
+        expect(Lunch.in_current_quarter).to_not include last_year_lunch
+      end
     end
   end
 end
