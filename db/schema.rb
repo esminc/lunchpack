df17d2eea669bb850f40fcbe4a75738bf6562a63
wai-doi <yusuke.doi@esm.co.jp>
2019-09-05 13:54:53 +0900

    Quarterモデルにユニーク制約をつけた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_043337) do
+ActiveRecord::Schema.define(version: 2019_09_05_044920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_09_05_043337) do
     t.date "end_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["period", "ordinal"], name: "index_quarters_on_period_and_ordinal", unique: true
   end
 
   create_table "users", force: :cascade do |t|
