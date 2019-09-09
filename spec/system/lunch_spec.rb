a17025175687006f2e7ef8719615510d64decfe3
wai-doi <yusuke.doi@esm.co.jp>
2019-09-09 14:59:18 +0900

    ランチ履歴を登録したユーザーだけその履歴を削除できる機能のテスト

--- spec/system/lunch_spec.rb
+++ spec/system/lunch_spec.rb
@@ -28,27 +28,38 @@ describe 'ランチ履歴の表示機能' do
   end
 end
 
-describe 'ランチ履歴の削除機能' do
+describe 'ランチ履歴を登録したユーザーだけその履歴を削除できる機能' do
   before do
-    members = [
-      create(:member, real_name: '鈴木一郎'),
-      create(:member, real_name: '鈴木二郎'),
-      create(:member, real_name: '鈴木三郎')
-    ]
+    create(:member, real_name: '鈴木一郎')
+    create(:member, real_name: '鈴木二郎')
+    create(:member, real_name: '鈴木三郎')
     sign_in create(:user)
     visit root_path
-    create_lunch(members, date: Date.new(2019,9,15))
+    find('.member-name', text: '鈴木一郎').click
+    find('.member-name', text: '鈴木二郎').click
+    find('.member-name', text: '鈴木三郎').click
+    within('.lunch-form') do
+      click_on('ランチに行く')
+    end
+    visit lunches_path
   end
 
-  it '削除ボタンを押すと履歴が削除できること' do
-    visit lunches_path
-    within('tr', text: '2019-09-15 鈴木一郎,鈴木二郎,鈴木三郎') do
+  it '自分の登録した履歴は削除ボタンから削除できること' do
+    within('tr', text: '鈴木一郎,鈴木二郎,鈴木三郎') do
       click_on('delete')
     end
     page.driver.browser.switch_to.alert.accept
-    expect(page).to have_content('2019-09-15 鈴木一郎,鈴木二郎,鈴木三郎の給付金利用履歴を削除しました')
+    expect(page).to have_content('鈴木一郎,鈴木二郎,鈴木三郎の給付金利用履歴を削除しました')
     within('table') do
-      expect(page).to_not have_content('2019-09-15 鈴木一郎,鈴木二郎,鈴木三郎')
+      expect(page).to_not have_content('鈴木一郎,鈴木二郎,鈴木三郎')
+    end
+  end
+
+  it '他人が登録した履歴には削除ボタンが無いこと' do
+    sign_in create(:user, email: 'other@esm.co.jp')
+    visit lunches_path
+    within('tr', text: '鈴木一郎,鈴木二郎,鈴木三郎') do
+      expect(page).to_not have_content('delete')
     end
   end
 end
