ae128207f51d29aa790f9e05408f99e29737862b
wai-doi <yusuke.doi@esm.co.jp>
2019-09-13 12:46:32 +0900

    メソッド名と変数名を修正

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -3,7 +3,7 @@ class Lunch < ApplicationRecord
   belongs_to :quarter
   has_and_belongs_to_many :members
   validate :must_have_benefits_available_count_members
-  validate :must_has_unique_trio_in_current_quarter
+  validate :must_go_to_lunch_with_members_who_did_not_go_together_during_same_quarter
 
   BENEFITS_AVAILABLE_MEMBERS_COUNT = 3
 
@@ -20,12 +20,12 @@ class Lunch < ApplicationRecord
   end
 
   # 同じクォーター内で同じメンバーの組み合わせでランチに行ってないことを検証
-  def must_has_unique_trio_in_current_quarter
+  def must_go_to_lunch_with_members_who_did_not_go_together_during_same_quarter
     lunches_in_same_quarter = Lunch.includes(:members).where(quarter: quarter)
     lunches_in_same_quarter.each do |lunch|
-      lunched_members = lunch.members & self.members
-      if lunched_members.size >= 2
-        errors.add(:went_to_lunch_in_same_quarter_and_members, "#{lunched_members.map(&:real_name).join(',')}は#{lunch.date}にランチ済みです")
+      members_who_went_to_lunch_together = lunch.members & self.members
+      if members_who_went_to_lunch_together.size >= 2
+        errors.add(:went_to_lunch_with_same_members, "#{members_who_went_to_lunch_together.map(&:real_name).join(',')}は#{lunch.date}にランチ済みです")
       end
     end
   end
