0d0652608d892b1b8e6f05868ae2c1dcc8039b12
wai-doi <yusuke.doi@esm.co.jp>
2019-10-09 15:48:20 +0900

    config.load_defaults 6.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Lunchpack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
