1b663bc0bded8fc11471638d706adc54efc5b76c
wai-doi <yusuke.doi@esm.co.jp>
2019-11-11 03:29:28 +0900

    ログイン時のフラッシュメッセージを変更

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      flash[:success] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
       session[:user_id] = @user.id
     else
