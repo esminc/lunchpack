8b746fe59429b3567f53e7d256c9ec61f4d35354
wai-doi <yusuke.doi@esm.co.jp>
2019-11-10 22:23:31 +0900

    テストを修正

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -17,17 +17,17 @@ describe 'プロジェクト管理機能' do
   describe '新規作成機能' do
     context '名前が入力される場合' do
       it '新規に追加できること' do
-        find('#new-btn').click
+        click_on 'プロジェクト追加'
         fill_in 'project[name]', with: 'プロダクト'
-        find('#submit-btn').click
+        click_on '登録する'
         expect(page).to have_content 'プロダクトを登録しました'
       end
     end
 
-    context '名前が入力されない場合' do
+    context '名前が入力されない場合', js: false do
       it '新規に追加できないこと' do
-        find('#new-btn').click
-        find('#submit-btn').click
+        click_on 'プロジェクト追加'
+        click_on '登録する'
         expect(page).to have_content 'プロジェクト名を入力してください'
       end
     end
@@ -35,17 +35,17 @@ describe 'プロジェクト管理機能' do
 
   describe '編集機能' do
     it '名前が編集できるか' do
-      find('.edit-btn').click
+      click_on '編集'
       fill_in 'project[name]', with: 'eiwasan'
-      find('#submit-btn').click
+      click_on '更新する'
       expect(page).to have_content 'eiwasanを更新しました'
     end
   end
 
   describe '削除機能' do
     it '削除できるか' do
-      find('.edit-btn').click
-      click_on('削除する')
+      click_on '編集'
+      click_on '削除する'
       page.driver.browser.switch_to.alert.accept
 
       expect(page).to have_content 'eiwakunを削除しました'
