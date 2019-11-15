1d7a0e4499cde9e2c68e465638c6b11e65ae837f
wai-doi <yusuke.doi@esm.co.jp>
2019-11-16 08:48:36 +0900

    名前をコピーできるボタンを設置

---
+++ app/assets/javascripts/clipboard_button.js
@@ -0,0 +1,32 @@
+document.addEventListener('turbolinks:load', function(){
+  // Tooltip
+  $('.clipboard-btn').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+  });
+
+  function setTooltip(btn, message) {
+    $(btn).tooltip('show')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  }
+
+  function hideTooltip(btn) {
+    setTimeout(function() {
+      $(btn).tooltip('hide');
+    }, 1000);
+  }
+
+  // Clipboard
+  var clipboard = new Clipboard('.clipboard-btn');
+
+  clipboard.on('success', function(e) {
+    setTooltip(e.trigger, 'Copied!');
+    hideTooltip(e.trigger);
+  });
+
+  clipboard.on('error', function(e) {
+    setTooltip(e.trigger, 'Failed!');
+    hideTooltip(e.trigger);
+  });
+});
