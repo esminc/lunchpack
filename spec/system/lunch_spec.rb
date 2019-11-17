28a51a8d905c234863da885e81a452af0f868685
wai-doi <yusuke.doi@esm.co.jp>
2019-11-18 00:10:08 +0900

    失敗するようになってたテストを修正

--- spec/system/lunch_spec.rb
+++ spec/system/lunch_spec.rb
@@ -21,12 +21,11 @@ describe 'ランチ履歴の表示機能' do
 
   it 'クオーターごとに履歴が表示される' do
     visit lunches_path
-
+    click_on('40期-1Q')
     expect(page).to have_content('2019-09-15 鈴木一郎,鈴木二郎,鈴木三郎')
     expect(page).to have_content('2019-09-16 鈴木一郎,鈴木四郎,鈴木五郎')
 
     click_on('40期-2Q')
-
     expect(page).to have_content('2019-12-15 鈴木一郎,鈴木二郎,鈴木三郎')
   end
 end
