52ca701f17e98eedf0347443f6cd76f11d5aaa29
wai-doi <yusuke.doi@esm.co.jp>
2019-11-19 11:21:31 +0900

    current_quarterの不要なテストを消した

--- spec/model/quarter_spec.rb
+++ spec/model/quarter_spec.rb
@@ -2,25 +2,10 @@ require 'rails_helper'
 
 describe Quarter do
   describe '.current_quarter' do
-    it '今日のクォーターがとれること' do
-      current_quarter = Quarter.current_quarter
-      expect(current_quarter.start_date .. current_quarter.end_date).to cover(Date.today)
-    end
+    subject(:current_quarter) { Quarter.current_quarter }
 
-    xit 'すでに今日のクォーターがDBにある場合、レコードが増えないこと' do
-      today = Date.new(2019, 9, 4)
-      Quarter.create!(
-        period: 40,
-        ordinal: 1,
-        start_date: Date.new(2019, 8, 1),
-        end_date: Date.new(2019, 10, 31)
-      )
-      expect { Quarter.current_quarter(today) }.to_not change(Quarter, :count)
-    end
-
-    xit 'すでに今日のクォーターがDBにない場合、レコードが増えること' do
-      today = Date.new(2019, 9, 4)
-      expect { Quarter.current_quarter(today) }.to change(Quarter, :count).by(1)
+    it '今日が含まれるクォーターが返されること' do
+      expect(current_quarter.start_date..current_quarter.end_date).to cover(Date.today)
     end
   end
 
