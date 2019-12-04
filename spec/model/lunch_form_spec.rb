c436040b338c7650e911d6ecfceaa8249947ca2d
wai-doi <yusuke.doi@esm.co.jp>
2019-12-04 12:12:03 +0900

    members_should_existのテスト(落ちるテストがあるので要修正)

--- spec/model/lunch_form_spec.rb
+++ spec/model/lunch_form_spec.rb
@@ -1,6 +1,4 @@
 require 'rails_helper'
-
-RSpec.describe LunchForm do
   describe '#must_have_benefits_available_count_members' do
     let(:lunch_form_params) { {date: '2019-11-21', members: members} }
 
@@ -14,6 +12,30 @@ RSpec.describe LunchForm do
 
     context '2 人指定されているとき' do
       let(:members) { ['111', 'bbb'] }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe '#members_should_exist' do
+    let(:lunch_form_params) { {date: '2019-11-21', members: members} }
+
+    subject { described_class.new(lunch_form_params) }
+
+    before do
+      create(:member, real_name: '鈴木一郎')
+      create(:member, real_name: '鈴木二郎')
+      create(:member, real_name: '鈴木三郎')
+    end
+
+    context '存在するメンバーの名前が渡されるとき' do
+      let(:members) { ['鈴木一郎', '鈴木二郎', '鈴木三郎'] }
+
+      it { is_expected.to be_valid }
+    end
+
+    context '存在しないメンバーの名前が渡されるとき' do
+      let(:members) { ['鈴木存在しない一郎', '鈴木二郎', '鈴木三郎'] }
 
       it { is_expected.not_to be_valid }
     end
