ed1f72b19f55acd8dc2168dc73826874163f5065
wai-doi <yusuke.doi@esm.co.jp>
2019-12-18 00:27:54 +0900

    todoからLayout/ExtraSpacingを消した

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -1,5 +1,5 @@
 class Lunch < ApplicationRecord
-  belongs_to :created_by,  foreign_key: :user_id, class_name: 'User'
+  belongs_to :created_by, foreign_key: :user_id, class_name: 'User'
   belongs_to :quarter
   has_and_belongs_to_many :members
 
