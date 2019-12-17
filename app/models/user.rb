637b4d58ebe4ae6ed63efc38ac13242252650f3c
wai-doi <yusuke.doi@esm.co.jp>
2019-12-18 00:53:09 +0900

    todoからLayout/HashAlignmentを消した

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,12 @@ class User < ApplicationRecord
     user = User.find_by(email: auth.info.email)
 
     unless user
-      user = User.create(email:     auth.info.email,
-                          provider: auth.provider,
-                          uid:      auth.uid,
-                          token:    auth.credentials.token,
-                          password: Devise.friendly_token[0, 20],
-                          meta:     auth.to_yaml)
+      user = User.create(email: auth.info.email,
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         token: auth.credentials.token,
+                         password: Devise.friendly_token[0, 20],
+                         meta: auth.to_yaml)
     end
     user
   end
