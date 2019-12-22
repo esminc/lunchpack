e6a0a937b0c21c6d4ce9fccab98d180aa2edeb5a
wai-doi <yusuke.doi@esm.co.jp>
2019-12-22 16:14:59 +0900

    todoからMetrics/MethodLengthを消した

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -12,10 +12,7 @@ class LunchesController < ApplicationController
     @lunch_form = LunchForm.new(lunch_form_params)
 
     if @lunch_form.valid?
-      members = Member.where(real_name: @lunch_form.members)
-      date = @lunch_form.date.to_date
-      quarter = Quarter.find_or_create_quarter(date)
-      lunch = quarter.lunches.create!(date: date, members: members, created_by: current_user)
+      lunch = create_lunch_with_quarter(@lunch_form)
       flash[:success] = t('dictionary.message.create.complete', resource_name: lunch.label_with_date_and_member_names)
       redirect_to lunches_url
     else
@@ -47,5 +44,12 @@ class LunchesController < ApplicationController
     @members = Member.includes(:projects).where(retired: false).order(:created_at)
     gon.lunch_trios = Quarter.current_quarter.lunches.includes(:members).map(&:members)
     gon.login_member = Member.find_by(email: current_user.email)
+  end
+
+  def create_lunch_with_quarter(lunch_form)
+    members = Member.where(real_name: lunch_form.members)
+    date = lunch_form.date.to_date
+    quarter = Quarter.find_or_create_quarter(date)
+    quarter.lunches.create!(date: date, members: members, created_by: current_user)
   end
 end
