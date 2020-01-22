f2fd59e81fda2f6cc4aa8c70e9b4442d8a0a84ea
wai-doi <yusuke.doi@esm.co.jp>
2020-01-23 02:05:43 +0900

    ランチのsystem specを改善した

--- spec/system/lunches/lunches_create_spec.rb
+++ spec/system/lunches/lunches_create_spec.rb
@@ -1,161 +1,247 @@
 require 'rails_helper'
 
 RSpec.describe 'ランチ履歴の登録機能', type: :system do
-  let!(:project) { create(:project) }
-  let!(:member1) { create(:member, real_name: '鈴木一郎', projects: [project]) }
-  let!(:member2) { create(:member, real_name: '鈴木二郎') }
-  let!(:member3) { create(:member, real_name: '鈴木三郎') }
-  let!(:login_user) { create(:user, email: 'login@esm.co.jp') }
+  let(:login_user) { create(:user, email: 'login@esm.co.jp') }
 
   before do
-    create(:member, real_name: '山田太郎', projects: [project])
-
     sign_in login_user
-    visit root_path
   end
 
-  describe 'メンバー選択リスト' do
-    it 'メンバーがリストに表示されること' do
-      expect(find('#members-list')).to have_content('鈴木一郎')
-      expect(find('#members-list')).to have_content('鈴木二郎')
-      expect(find('#members-list')).to have_content('鈴木三郎')
-    end
-  end
-
-  describe 'メンバーを選択する' do
-    it '名前をクリックすると枠に名前が移動すること' do
-      find('.member-name', text: '山田太郎').click
-
-      expect(first('.member-form').value).to eq '山田太郎'
-    end
-
-    context '同じプロジェクトに所属するメンバー同士の組み合わせの場合' do
+  describe '表からメンバーを選択する機能' do
+    describe '一度選択しても戻せるしくみ' do
       before do
-        find('.member-name', text: '山田太郎').click
+        create(:member, real_name: '鈴木一郎')
+        create(:member, real_name: '鈴木二郎')
+        create(:member, real_name: '鈴木三郎')
       end
 
-      it '〜と同じプロジェクトという理由が表示がされること' do
-        within('.member-row', text: '鈴木一郎') do
-          expect(page).to have_content('山田太郎と同じプロジェクト')
-        end
-      end
-
-      it 'クリックしても名前がフォームに移動しないこと' do
-        find('.member-row', text: '鈴木一郎').click
-
-        within('.lunch-form') do
-          expect(page).not_to have_content('鈴木一郎')
-        end
-      end
-    end
-
-    context 'すでにランチに行っているメンバー同士の組み合わせを選択する場合' do
-      before do
-        create_lunch([member1, member2, member3], login_user)
-
+      it '名前が入ったフォームをクリックすると表に名前が戻ること' do
         visit root_path
-      end
 
-      context 'ランチに行ったクウォーターと同じ期間の場合' do
-        it '〜とランチ済みという理由が表示がされること' do
-          find('.member-name', text: '鈴木一郎').click
-
-          within('.member-row', text: '鈴木二郎') do
-            expect(page).to have_content('鈴木一郎とランチ済み')
-          end
-          within('.member-row', text: '鈴木三郎') do
-            expect(page).to have_content('鈴木一郎とランチ済み')
-          end
-        end
-
-        it 'クリックしても名前がフォームに移動しないこと' do
+        within('table') do
+          find('.member-row', text: '鈴木一郎').click
           find('.member-row', text: '鈴木二郎').click
-
-          within('.lunch-form') do
-            expect(page).not_to have_content('鈴木二郎')
-          end
-
           find('.member-row', text: '鈴木三郎').click
 
-          within('.lunch-form') do
-            expect(page).not_to have_content('鈴木三郎')
+          expect(page).not_to have_content '鈴木一郎'
+          expect(page).not_to have_content '鈴木二郎'
+          expect(page).not_to have_content '鈴木三郎'
+        end
+
+        find('label', text: '１人目').click
+        find('label', text: '２人目').click
+        find('label', text: '３人目').click
+
+        within('table') do
+          expect(page).to have_content '鈴木一郎'
+          expect(page).to have_content '鈴木二郎'
+          expect(page).to have_content '鈴木三郎'
+        end
+      end
+    end
+
+    describe '3人まで選択できるしくみ' do
+      before do
+        create(:member, real_name: '鈴木一郎')
+        create(:member, real_name: '鈴木二郎')
+        create(:member, real_name: '鈴木三郎')
+        create(:member, real_name: '鈴木四郎')
+      end
+
+      it '名前をクリックすると3人までフォームに名前が移動すること' do
+        visit root_path
+
+        within('table') do
+          find('.member-row', text: '鈴木一郎').click
+          find('.member-row', text: '鈴木二郎').click
+          find('.member-row', text: '鈴木三郎').click
+          find('.member-row', text: '鈴木四郎').click
+
+          expect(page).not_to have_content '鈴木一郎'
+          expect(page).not_to have_content '鈴木二郎'
+          expect(page).not_to have_content '鈴木三郎'
+          expect(page).to have_content '鈴木四郎'
+        end
+
+        expect(page).to have_field '１人目', with: '鈴木一郎'
+        expect(page).to have_field '２人目', with: '鈴木二郎'
+        expect(page).to have_field '３人目', with: '鈴木三郎'
+      end
+    end
+
+    describe '選択できるメンバーの制限について' do
+      context '同じプロジェクトに所属するメンバー同士を選ぶ場合' do
+        let(:project) { create(:project) }
+
+        before do
+          create(:member, real_name: '鈴木一郎', projects: [project])
+          create(:member, real_name: '鈴木二郎', projects: [project])
+        end
+
+        it '選択できない理由が表示され、クリックしても名前がフォームに移動しないこと' do
+          visit root_path
+
+          within('table') do
+            find('.member-row', text: '鈴木一郎').click
+
+            expect(find('.member-row', text: '鈴木二郎')).to have_content '鈴木一郎と同じプロジェクト'
+
+            find('.member-row', text: '鈴木二郎').click
           end
+
+          expect(page).to have_field '１人目', with: '鈴木一郎'
+          expect(page).to have_field '２人目', with: ''
         end
       end
 
-      context 'ランチに行ったクウォーターと違う期間の場合' do
+      context 'すでにランチに行っているメンバー同士を選ぶ場合' do
         before do
-          travel 3.months
-          visit root_path
+          members = [
+            create(:member, real_name: '鈴木一郎'),
+            create(:member, real_name: '鈴木二郎'),
+            create(:member, real_name: '鈴木三郎')
+          ]
+          create_lunch(members, login_user, date: lunch_date)
         end
 
-        it '〜とランチ済みという理由が表示がないこと' do
-          find('.member-name', text: '鈴木一郎').click
+        context 'すでに行ったランチが現在のクウォーターと同じ期間の場合' do
+          let(:lunch_date) { Date.current }
 
-          within('.member-row', text: '鈴木二郎') do
-            expect(page).not_to have_content('鈴木一郎とランチ済み')
-          end
-          within('.member-row', text: '鈴木三郎') do
-            expect(page).not_to have_content('鈴木一郎とランチ済み')
+          it '選択できない理由が表示され、クリックしても名前がフォームに移動しないこと' do
+            visit root_path
+
+            within('table') do
+              find('.member-row', text: '鈴木一郎').click
+
+              expect(find('.member-row', text: '鈴木二郎')).to have_content '鈴木一郎とランチ済み'
+              expect(find('.member-row', text: '鈴木三郎')).to have_content '鈴木一郎とランチ済み'
+
+              find('.member-row', text: '鈴木二郎').click
+              find('.member-row', text: '鈴木三郎').click
+            end
+
+            expect(page).to have_field '２人目', with: ''
+            expect(page).to have_field '３人目', with: ''
           end
         end
+
+        context 'すでに行ったランチが現在のクウォーターと違う期間の場合' do
+          let(:lunch_date) { 3.months.ago }
+
+          it '選択できない理由が表示されず、クリックすると名前がフォームに移動すること' do
+            visit root_path
+
+            within('table') do
+              find('.member-row', text: '鈴木一郎').click
+
+              expect(find('.member-row', text: '鈴木二郎')).not_to have_content '鈴木一郎とランチ済み'
+              expect(find('.member-row', text: '鈴木三郎')).not_to have_content '鈴木一郎とランチ済み'
+
+              find('.member-row', text: '鈴木二郎').click
+              find('.member-row', text: '鈴木三郎').click
+            end
+
+            expect(page).to have_field '２人目', with: '鈴木二郎'
+            expect(page).to have_field '３人目', with: '鈴木三郎'
+          end
+        end
+      end
+    end
+  end
+
+  describe 'ログインユーザーの名前がフォームの一人目に入力されるしくみ' do
+    context 'ログインユーザーと同じメールアドレスをもつメンバーが登録されている場合' do
+      before do
+        create(:member, real_name: 'ろぐいん太郎', email: login_user.email)
+      end
+
+      it 'フォームの１人目にログインユーザーが入力されていること' do
+        visit root_path
+
+        expect(page).to have_field '１人目', with: 'ろぐいん太郎'
       end
     end
   end
 
   describe 'ランチに行ったことの登録機能' do
-    context '正しい入力をする場合' do
+    before do
+      create(:member, real_name: '鈴木一郎')
+      create(:member, real_name: '鈴木二郎')
+      create(:member, real_name: '鈴木三郎')
+    end
+
+    describe '日付の入力' do
       context '今日の日付で登録する場合' do
-        let(:date) { Date.current }
+        let(:lunch_date) { Date.current }
 
         it '正しく登録されたことのメッセージが表示されること' do
-          find('.member-name', text: '鈴木一郎').click
-          find('.member-name', text: '鈴木二郎').click
-          find('.member-name', text: '鈴木三郎').click
-          fill_in '行った日', with: date
-          find('#submit-btn').click
+          visit root_path
 
-          expect(page).to have_content '鈴木一郎,鈴木二郎,鈴木三郎の給付金利用履歴を登録しました'
+          within('table') do
+            find('.member-row', text: '鈴木一郎').click
+            find('.member-row', text: '鈴木二郎').click
+            find('.member-row', text: '鈴木三郎').click
+          end
+
+          fill_in '行った日', with: lunch_date
+          click_on '登録する'
+
+          expect(page).to have_content "#{lunch_date} 鈴木一郎,鈴木二郎,鈴木三郎の給付金利用履歴を登録しました"
         end
       end
 
       context '前の期の日付で登録する場合' do
-        let(:date) { Date.current.prev_month(3) }
+        let(:lunch_date) { Date.current.prev_month(3) }
 
         it '正しく登録されたことのメッセージが表示されること' do
-          find('.member-name', text: '鈴木一郎').click
-          find('.member-name', text: '鈴木二郎').click
-          find('.member-name', text: '鈴木三郎').click
-          fill_in '行った日', with: date
-          find('#submit-btn').click
+          visit root_path
 
-          expect(page).to have_content "#{Date.current.prev_month(3)} 鈴木一郎,鈴木二郎,鈴木三郎の給付金利用履歴を登録しました"
+          within('table') do
+            find('.member-row', text: '鈴木一郎').click
+            find('.member-row', text: '鈴木二郎').click
+            find('.member-row', text: '鈴木三郎').click
+          end
+
+          fill_in '行った日', with: lunch_date
+          click_on '登録する'
+
+          expect(page).to have_content "#{lunch_date} 鈴木一郎,鈴木二郎,鈴木三郎の給付金利用履歴を登録しました"
+        end
+      end
+
+      context '日付を空にする場合' do
+        it 'エラーメッセージが表示されること' do
+          visit root_path
+
+          within('table') do
+            find('.member-row', text: '鈴木一郎').click
+            find('.member-row', text: '鈴木二郎').click
+            find('.member-row', text: '鈴木三郎').click
+          end
+
+          fill_in '行った日', with: nil
+          click_on '登録する'
+
+          expect(page).to have_content '日付を入力してください'
         end
       end
     end
 
-    context '不正な入力をする場合' do
-      it 'エラーメッセージが表示されること' do
-        find('.member-name', text: '鈴木一郎').click
-        find('.member-name', text: '鈴木二郎').click
-        fill_in '行った日', with: nil
-        find('#submit-btn').click
+    describe 'メンバーの入力' do
+      context '3人メンバーを選ばない場合' do
+        it 'エラーメッセージが表示されること' do
+          visit root_path
 
-        expect(page).to have_content '3人のメンバーを入力してください'
-        expect(page).to have_content '日付を入力してください'
+          within('table') do
+            find('.member-row', text: '鈴木一郎').click
+            find('.member-row', text: '鈴木二郎').click
+          end
+
+          click_on '登録する'
+
+          expect(page).to have_content '3人のメンバーを入力してください'
+        end
       end
-    end
-  end
-
-  describe 'ログインしているユーザーが自動でフォームの一人目に入力される機能' do
-    before do
-      create(:member, real_name: 'ろぐいん', email: 'login@esm.co.jp')
-
-      visit root_path
-    end
-
-    it 'フォームの一人目にログインユーザーが入力されているか' do
-      expect(first('.member-form').value).to eq 'ろぐいん'
     end
   end
 end
--- spec/system/lunches/lunches_index_spec.rb
+++ spec/system/lunches/lunches_index_spec.rb
@@ -10,22 +10,26 @@ RSpec.describe 'ランチ履歴の表示機能', type: :system do
     trio1 = [member1, member2, member3]
     trio2 = [member1, member4, member5]
 
-    login_user = create(:user)
-    create_lunch(trio1, login_user, date: Date.new(2019, 9, 15))
-    create_lunch(trio2, login_user, date: Date.new(2019, 9, 16))
-    create_lunch(trio1, login_user, date: Date.new(2019, 12, 15))
+    user = create(:user)
+    create_lunch(trio1, user, date: Date.new(2019, 9, 15))
+    create_lunch(trio2, user, date: Date.new(2019, 9, 16))
+    create_lunch(trio1, user, date: Date.new(2019, 12, 15))
 
-    sign_in login_user
-    visit root_path
+    sign_in user
   end
 
-  it 'クオーターごとに履歴が表示される' do
+  it 'クオーターごとに履歴が表示されること' do
     visit lunches_path
-    click_on('40期-1Q')
-    expect(page).to have_content('2019-09-15 鈴木一郎,鈴木二郎,鈴木三郎')
-    expect(page).to have_content('2019-09-16 鈴木一郎,鈴木四郎,鈴木五郎')
 
-    click_on('40期-2Q')
-    expect(page).to have_content('2019-12-15 鈴木一郎,鈴木二郎,鈴木三郎')
+    click_on '40期-1Q'
+    within('table') do
+      expect(page).to have_content '2019-09-15 鈴木一郎,鈴木二郎,鈴木三郎'
+      expect(page).to have_content '2019-09-16 鈴木一郎,鈴木四郎,鈴木五郎'
+    end
+
+    click_on '40期-2Q'
+    within('table') do
+      expect(page).to have_content '2019-12-15 鈴木一郎,鈴木二郎,鈴木三郎'
+    end
   end
 end
